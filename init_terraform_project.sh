12dc636be83bbb05670826fb5abb410cf3a5efe7
jacubillos10 <alejandro.cubillos@universitasxxi.com>
2026-02-27 10:58:09 -0500

    Created an script to make my life just a little bit easier

---
+++ init_terraform_project.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+project_name=$1
+region=$2
+
+example_module_path=${project_name}/example_module
+
+mkdir -p ${example_module_path}
+
+generate_provider_aws() {
+    cat <<-EOF > ${project_name}/providers.tf
+	terraform {
+	  required_providers {
+	    aws = {
+	      source  = "hashicorp/aws"
+	      version = "6.31.0"
+	    }
+	  }
+	}
+
+	provider "aws" {
+	  region = "${region}"
+	}
+	EOF
+}
+
+touch_files() {
+    touch "$1/main.tf" "$1/variables.tf" "$1/outputs.tf"
+}
+
+touch_files ${project_name}
+touch_files ${example_module_path}
+touch ${project_name}/terraform.tfvars
+generate_provider_aws
+
+curl -sSLo -o "${project_name}/.gitignore" https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Terraform.gitignore
+
+cd "${project_name}" && git init
